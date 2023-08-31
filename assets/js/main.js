d1807b90be1756f604b6f0b78ffedb3ca663c124
JadsonPS <jadsonps12345@gmail.com>
2023-08-31 14:48:09 -0300

    Adições de descrição dos pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const descriptionPokemon = document.getElementById('description')
 
 const maxRecords = 151
 const limit = 10
@@ -7,7 +8,8 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+    <a href="#description">
+        <li class="pokemon ${pokemon.type}" onclick="carregarDescripion(${pokemon.number})">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -20,6 +22,7 @@ function convertPokemonToLi(pokemon) {
                      alt="${pokemon.name}">
             </div>
         </li>
+    </a>
     `
 }
 
@@ -45,3 +48,56 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+
+
+
+
+function carregarDescripion(pokemonEspecificoId) {
+     
+     
+    const pokemonEscolhido = fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonEspecificoId}`)
+       .then((response) => response.json())
+       .then((pokemonJsonDetail) => {
+           console.log(pokemonJsonDetail)
+       
+           const newDescricao = `
+
+           <div id="description" class="${pokemonJsonDetail.types[0].type.name}">
+           <div class="info ${pokemonJsonDetail.types[0].type.name}">
+               <div>
+                   <div class="tipo-numero">
+                       <p>${pokemonJsonDetail.types[0].type.name}</p>
+                       <p>#${pokemonJsonDetail.id}</p>   
+                   </div>
+                   <h2>${pokemonJsonDetail.name}</h2>
+                  
+                   <ul class="caracteristicas">
+                       <li><div class="espaco-texto-lista">Height:</div>  <span> ${pokemonJsonDetail.height/10}m</span></li>
+                       <li><div class="espaco-texto-lista">Weight:</div>  <span> ${pokemonJsonDetail.weight/10}kg</span></li>
+                       <li><div class="espaco-texto-lista">Abilities:</div>  <span> ${pokemonJsonDetail.abilities[0].ability.name}</span></li>
+                   </ul>
+               </div>
+
+               
+           </div>
+           <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonJsonDetail.id}.svg"
+                   alt="Foto do ${pokemonJsonDetail.name}">
+           <div class="stats ${pokemonJsonDetail.types[0].type.name}">
+
+               <ul>
+                   <li><div class="stats-dados">HP:</div>  <div id="hp" class="bar-stats" style="width:${pokemonJsonDetail.stats[0].base_stat}px;"></div><span> ${pokemonJsonDetail.stats[0].base_stat}</span></li>
+                   <li><div class="stats-dados">Attack:</div>  <div id="atack" class="bar-stats" style="width:${pokemonJsonDetail.stats[1].base_stat}px;"></div><span> ${pokemonJsonDetail.stats[1].base_stat}</span></li>
+                   <li><div class="stats-dados">Defense:</div>  <div id="defense" class="bar-stats" style="width:${pokemonJsonDetail.stats[2].base_stat}px;"></div><span> ${pokemonJsonDetail.stats[2].base_stat}</span></li>
+                   <li><div class="stats-dados">Special Attack:</div>  <div id="atack" class="bar-stats" style="width:${pokemonJsonDetail.stats[3].base_stat}px;"></div><span> ${pokemonJsonDetail.stats[3].base_stat}</span></li>
+                   <li><div class="stats-dados">Special Defense:</div>  <div id="hp" class="bar-stats" style="width:${pokemonJsonDetail.stats[4].base_stat}px;"></div><span> ${pokemonJsonDetail.stats[4].base_stat}</span></li>
+                   <li><div class="stats-dados">Speed:</div>  <div id="atack" class="bar-stats" style="width:${pokemonJsonDetail.stats[5].base_stat}px;"></div><span> ${pokemonJsonDetail.stats[5].base_stat}</span></li>
+                 </ul>
+
+           </div>
+           </div>
+           `
+           
+           descriptionPokemon.innerHTML = newDescricao;
+       })
+}
